d9855cb129e512c0e4806350fb4793877ab04283
Юлия Жук <sheburova801@mail.ru>
2022-10-08 16:57:36 +0300

    1.py

---
+++ 1.py
@@ -0,0 +1,28 @@
+#Вычислить число c заданной точностью *d*
+#Пример:
+#при d = 0.001, π = 3.141
+#при d = 0.1, π = 3.1
+#при d = 0.00001, π = 3.14154
+#d от 0.1 до 0.0000000001
+
+def InputNumbers(inputText):
+    is_OK = False
+    while not is_OK:
+        try:
+            number = float(input(f"{inputText}"))
+            is_OK = True
+        except ValueError:
+            print("Какое-то неправильное число!")
+    return number
+
+
+def division(num1, num2):
+    return num1 / num2
+
+
+num1 = InputNumbers("Введите 1 число: ")
+num2 = InputNumbers("Введите 2 число: ")
+n = int(InputNumbers("Введите точность (количество знаков после запятой): "))
+rezult = round(division(num1, num2), n)
+
+print(f"Результат деления: {rezult}")
